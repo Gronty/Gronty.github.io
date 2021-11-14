6953779d4abedbbf341664391a67472235b29217
Gron <hec.gron@gmail.com>
2021-11-14 16:29:03 +0100

    static site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  applicationId:'3REQOHSTBQ',
+  adminApiKey: '79c2dc2c114e92111e06a4382d765781',
+  indexName: 'Interview Candidate'
 };
